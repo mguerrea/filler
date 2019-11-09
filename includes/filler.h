8dcfcd53d3ee142cd5fd8d94fd3c38aebe3f8f51
Morgane GUERREAU <mguerrea@student.42.fr>
2019-11-09 13:22:42 +0100

    norm

--- includes/filler.h
+++ includes/filler.h
@@ -6,7 +6,7 @@
 /*   By: mguerrea <mguerrea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/29 18:36:29 by mguerrea          #+#    #+#             */
-/*   Updated: 2019/10/30 16:24:42 by mguerrea         ###   ########.fr       */
+/*   Updated: 2019/11/09 13:21:08 by mguerrea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 # include "get_next_line.h"
 # include <stdio.h>
 
-#define RED     "\033[31m"
-#define RESET   "\033[0m"
+# define RED     "\033[31m"
+# define RESET   "\033[0m"
 
 typedef struct	s_pos
 {
@@ -25,23 +25,23 @@ typedef struct	s_pos
 	int y;
 }				t_pos;
 
-typedef struct s_map
+typedef struct	s_map
 {
-	char **tab;
-	int x;
-	int y;
-	char me;
-	char they;
-	t_pos last;
+	char	**tab;
+	int		x;
+	int		y;
+	char	me;
+	char	they;
+	t_pos	last;
 }				t_map;
 
-void place_piece(t_map *map, t_map *piece);
-void print_map(t_map *map);
-void get_last(t_map *oldmap, t_map *map);
-void get_player(char *str, t_map *map);
-t_map *get_piece(void);
-t_map *get_map(void);
-void free_map(t_map *map);
-void print_pos(t_pos *res);
+void			place_piece(t_map *map, t_map *piece);
+void			print_map(t_map *map);
+void			get_last(t_map *oldmap, t_map *map);
+void			get_player(char *str, t_map *map);
+t_map			*get_piece(void);
+t_map			*get_map(void);
+void			free_map(t_map *map);
+void			print_pos(t_pos *res);
 
 #endif
